ffa39c4c117b062b496c730abd86bc674f1fa8c1
idle sign <idlesign@yandex.ru>
2014-08-28 12:53:43 +0700

    More refactoring and docstrings.

--- admirarchy/utils.py
+++ admirarchy/utils.py
@@ -9,56 +9,46 @@ from django.contrib.admin.options import ModelAdmin
 from .exceptions import AdmirarchyConfigurationError
 
 
-class HierarchicalChangeList(ChangeList):
-
-    def __init__(self, *args, **kwargs):
-        self.hierarchy = args[-1].hierarchy  # model_admin
-        if not isinstance(self.hierarchy, NoHierarchy):
-            # Add hierarchy navigation column.
-            args = list(args)
-            args[2] = [self.hierarchy.NAV_FIELD_MARKER] + list(args[2])  # list_display
-        super(HierarchicalChangeList, self).__init__(*args)
-
-    def get_queryset(self, request):
-        self.hierarchy.update_changelist(self, request=request)
-        return super(HierarchicalChangeList, self).get_queryset(request)
-
-    def get_results(self, request):
-        super(HierarchicalChangeList, self).get_results(request)
-        if self.hierarchy:
-            self.result_list = self.hierarchy.get_updated_changelist_results(self.result_list, model=self.model)
-
-    def check_field_exists(self, field_name):
-        if settings.DEBUG:
-            try:
-                self.lookup_opts.get_field_by_name(field_name)
-            except FieldDoesNotExist as e:
-                raise AdmirarchyConfigurationError(e)
-
-
 class HierarchicalModelAdmin(ModelAdmin):
+    """Customized Model admin handling hierarchies navigation."""
 
     hierarchy = False
     change_list_template = 'admin/admirarchy/change_list.html'
 
     def get_changelist(self, request, **kwargs):
+        """Returns an appropriate ChangeList for ModelAdmin.
+
+        Initializes hierarchies handling.
+
+        :param request:
+        :param kwargs:
+        :return:
+        """
         Hierarchy.init_hierarchy(self)
         return HierarchicalChangeList
 
     def change_view(self, *args, **kwargs):
-        if self.hierarchy:
-            self.raw_id_fields += ('parent',)
+        """Renders detailed model edit page."""
+        self.hierarchy.hook_change_view(self, args, kwargs)
         return super(HierarchicalModelAdmin, self).change_view(*args, **kwargs)
 
     def action_checkbox(self, obj):
+        """Renders checkboxes.
+
+        Disable checkbox for parent item navigation link.
+
+        """
         if getattr(obj, Hierarchy.UPPER_LEVEL_LINK_MARKER, False):
             return ''
         return super(HierarchicalModelAdmin, self).action_checkbox(obj)
 
     def hierarchy_nav(self, obj):
-        result_repr = ''  # No children.
+        """Renders hierarchy navigation elements (folders)."""
+
+        result_repr = ''  # For items without children.
         ch_count = getattr(obj, Hierarchy.CHILD_COUNT_MODEL_ATTR, 0)
-        if ch_count:  # Has children
+
+        if ch_count:  # For items with children.
             icon = 'icon icon-folder'
             title = _('Objects inside: %s') % ch_count
             if getattr(obj, Hierarchy.UPPER_LEVEL_LINK_MARKER, False):
@@ -67,10 +57,57 @@ class HierarchicalModelAdmin(ModelAdmin):
             url = './'
             if obj.pk:
                 url = '?pid=%s' % obj.pk
+
             #url = add_preserved_filters({'preserved_filters': changelist.preserved_filters, 'opts': changelist.opts}, url)
             result_repr = format_html('<a href="{0}" class="{1}" title="{2}"></a>', url, icon, title)
         return result_repr
     hierarchy_nav.short_description = ''
+
+
+class HierarchicalChangeList(ChangeList):
+    """Customized ChangeList used by HierarchicalModelAdmin to handle hierarchies."""
+
+    def __init__(self, *args):
+        """Adds hierarchy navigation column if necessary.
+
+        :param args:
+        :return:
+        """
+        self.hierarchy = args[-1].hierarchy  # model_admin
+        if not isinstance(self.hierarchy, NoHierarchy):
+            args = list(args)
+            args[2] = [self.hierarchy.NAV_FIELD_MARKER] + list(args[2])  # list_display
+        super(HierarchicalChangeList, self).__init__(*args)
+
+    def get_queryset(self, request):
+        """Constructs a query set.
+
+        :param request:
+        :return:
+        """
+        self.hierarchy.hook_get_queryset(self, request)
+        return super(HierarchicalChangeList, self).get_queryset(request)
+
+    def get_results(self, request):
+        """Gets query set results.
+
+        :param request:
+        :return:
+        """
+        super(HierarchicalChangeList, self).get_results(request)
+        self.hierarchy.hook_get_results(self)
+
+    def check_field_exists(self, field_name):
+        """Implements field exists check for debugging purposes.
+
+        :param field_name:
+        :return:
+        """
+        if settings.DEBUG:
+            try:
+                self.lookup_opts.get_field_by_name(field_name)
+            except FieldDoesNotExist as e:
+                raise AdmirarchyConfigurationError(e)
 
 
 ########################################################
@@ -79,34 +116,45 @@ class HierarchicalModelAdmin(ModelAdmin):
 class Hierarchy(object):
 
     PARENT_ID_QS_PARAM = 'pid'  # Parent ID query string parameter.
-    CHILD_COUNT_MODEL_ATTR = 'child_count'
+    CHILD_COUNT_MODEL_ATTR = 'child_count'  # Attribute given to every model.
     UPPER_LEVEL_LINK_MARKER = 'dummy'
     NAV_FIELD_MARKER = 'hierarchy_nav'
 
     @classmethod
     def init_hierarchy(cls, model_admin):
+        """Initializes model admin with hierarchy data."""
         hierarchy = getattr(model_admin, 'hierarchy')
         if hierarchy:
             if not isinstance(hierarchy, Hierarchy):
-                hierarchy = AdjacencyList()
+                hierarchy = AdjacencyList()  # For `True` and etc. TODO heuristics maybe.
         else:
             hierarchy = NoHierarchy()
         model_admin.hierarchy = hierarchy
 
-    def get_updated_changelist_results(self, result_list, model):
-        return result_list
+    @classmethod
+    def get_pid_from_request(cls, changelist, request):
+        """Gets parent ID from query string.
 
-    def update_changelist(self, changelist, request):
-        changelist.hierarchy = self
-        param_value = None
-        if not isinstance(self, NoHierarchy):
-            val = request.GET.get(self.PARENT_ID_QS_PARAM, False)
-            param_value = val or None
-            try:
-                del changelist.params[self.PARENT_ID_QS_PARAM]
-            except KeyError:
-                pass
-        return param_value
+        :param changelist:
+        :param request:
+        :return:
+        """
+        val = request.GET.get(cls.PARENT_ID_QS_PARAM, False)
+        pid = val or None
+        try:
+            del changelist.params[cls.PARENT_ID_QS_PARAM]
+        except KeyError:
+            pass
+        return pid
+
+    def hook_change_view(self, changelist, view_args, view_kwargs):
+        """Triggered by `ModelAdmin.change_view()`."""
+
+    def hook_get_results(self, changelist):
+        """Triggered by `ChangeList.get_results()`."""
+
+    def hook_get_queryset(self, changelist, request):
+        """Triggered by `ChangeList.get_queryset()`."""
 
 
 class NoHierarchy(Hierarchy):
@@ -119,24 +167,35 @@ class AdjacencyList(Hierarchy):
         self.pid_field = parent_id_field
         self.pid_field_real = '%s_id' % parent_id_field
 
-    def update_changelist(self, changelist, request):
-        changelist.check_field_exists(self.pid_field)
-        param_value = super(AdjacencyList, self).update_changelist(changelist, request)
-        changelist.params[self.pid_field] = param_value
-        return param_value
+    def hook_change_view(self, changelist, view_args, view_kwargs):
+        """Triggered by `ModelAdmin.change_view()`.
 
-    def get_updated_changelist_results(self, result_list, model):
+        Replaces parent item dropdown list with a lookup dialog.
+
+        """
+        # TODO Implement in-popups functioning.
+        changelist.raw_id_fields += (self.pid_field,)
+
+    def hook_get_queryset(self, changelist, request):
+        """Triggered by `ChangeList.get_queryset()`."""
+        changelist.check_field_exists(self.pid_field)
+        pid = self.get_pid_from_request(changelist, request)
+        changelist.params[self.pid_field] = pid
+        return pid
+
+    def hook_get_results(self, changelist):
+        """Triggered by `ChangeList.get_results()`."""
         get_parent = lambda m: getattr(m, self.pid_field_real, None)
-        result_list = list(result_list)
+        result_list = list(changelist.result_list)
         parent_id = get_parent(result_list[0])
         if parent_id:
-            parent = model.objects.get(pk=parent_id)
-            parent = model(pk=get_parent(parent))
+            parent = changelist.model.objects.get(pk=parent_id)
+            parent = changelist.model(pk=get_parent(parent))
             setattr(parent, self.CHILD_COUNT_MODEL_ATTR, 1)
             setattr(parent, self.UPPER_LEVEL_LINK_MARKER, True)
             result_list = [parent] + result_list
         kwargs_filter = {'%s__in' % self.pid_field: result_list}
-        stats_qs = model.objects.filter(**kwargs_filter).values_list(self.pid_field).annotate(cnt=models.Count(self.pid_field))
+        stats_qs = changelist.model.objects.filter(**kwargs_filter).values_list(self.pid_field).annotate(cnt=models.Count(self.pid_field))
         stats = {item[0]: item[1] for item in stats_qs}
         for item in result_list:
             if not hasattr(item, self.CHILD_COUNT_MODEL_ATTR):
@@ -144,4 +203,4 @@ class AdjacencyList(Hierarchy):
                     setattr(item, self.CHILD_COUNT_MODEL_ATTR, stats[item.id])
                 except KeyError:
                     setattr(item, self.CHILD_COUNT_MODEL_ATTR, 0)
-        return result_list
+        changelist.result_list = result_list
