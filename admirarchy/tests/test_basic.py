38792c88651428deb03499ff10d3e659077f1791
idle sign <idlesign@yandex.ru>
2018-09-07 19:26:27 +0700

    Prepared to use pytest-djangoapp.

---
+++ admirarchy/tests/test_basic.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+def test_stub():
+    assert True
+
--- setup.py
+++ setup.py
@@ -1,31 +1,51 @@
+import io
 import os
-from setuptools import setup
-from admirarchy import VERSION
-
+import re
+import sys
+from setuptools import setup, find_packages
 
 PATH_BASE = os.path.dirname(__file__)
 
-f = open(os.path.join(PATH_BASE, 'README.rst'))
-README = f.read()
-f.close()
+
+def read_file(fpath):
+    """Reads a file within package directories."""
+    with io.open(os.path.join(PATH_BASE, fpath)) as f:
+        return f.read()
+
+
+def get_version():
+    """Returns version number, without module import (which can lead to ImportError
+    if some dependencies are unavailable before install."""
+    contents = read_file(os.path.join('admirarchy', '__init__.py'))
+    version = re.search('VERSION = \(([^)]+)\)', contents)
+    version = version.group(1).replace(', ', '.').strip()
+    return version
+
 
 setup(
     name='django-admirarchy',
-    version='.'.join(map(str, VERSION)),
+    version=get_version(),
     url='https://github.com/idlesign/django-admirarchy',
 
     description='Django Admin addon to navigate through hierarchies',
-    long_description=README,
+    long_description=read_file('README.rst'),
     license='BSD 3-Clause License',
 
     author='Igor `idle sign` Starikov',
     author_email='idlesign@yandex.ru',
 
-    packages=['admirarchy'],
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
 
     install_requires=[],
+    setup_requires=[] + (['pytest-runner'] if 'test' in sys.argv else []),
+
+    test_suite='tests',
+    tests_require=[
+        'pytest',
+        'pytest-djangoapp>=0.8.0',
+    ],
 
     classifiers=[
         # As in https://pypi.python.org/pypi?:action=list_classifiers
