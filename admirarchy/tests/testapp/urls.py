2b99b8cf5b7a8109d7933255cb2a7c04881be57c
idle sign <idlesign@yandex.ru>
2021-12-18 10:52:11 +0700

    Test update.

--- admirarchy/tests/testapp/urls.py
+++ admirarchy/tests/testapp/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from django.conf.urls import url
+
+try:
+    from django.urls import re_path
+
+except ImportError:
+    from django.conf.urls import url as re_path
 
 from pytest_djangoapp.compat import get_urlpatterns
 
 
 urlpatterns = get_urlpatterns([
-    url('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
 ])
